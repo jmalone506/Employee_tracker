d794622957ce54fe1989a4c181040bd23c1e8317
Joshua Malone <jcharlesmalone@gmail.com>
2021-02-15 13:49:07 -0800

    "pushed"
    ~

--- 02-Homework/db/schema.sql
+++ 02-Homework/db/schema.sql
@@ -26,3 +26,43 @@ CREATE TABLE employee (
     manager_id INT NULL
     PRIMARY KEY (id)
 );
+INSERT INTO department (dept)
+VALUES 
+("MANAGEMENT"),
+("DOCTORS"),
+("DOCTOR ASSISTANTS"),
+("THERAPIST"),
+("RECEPTION")
+;
+
+INSERT INTO job (title,salary,department_id)
+VALUES
+("OFFICE MANAGER",80000, 1),
+("GENERAL MANAGER", 80000, 1),
+("OPERATIONS MANAGER", 65000, 1),
+("HUMAN RESOURCES", 58000, 1),
+("CHIROPRACTOR", 72000, 2),
+("MEDICAL", 90000, 2),
+("CHIROPRACTORS ASSISTANT", 42000, 3),
+("MEDICAL ASSISTANT", 42000, 3),
+("MASSAGE THERAPIST" 42000, 4),
+("FRONT DESK RECEPTIONIST" 46000, 5),
+
+INSERT INTO employee (first_name,last_name,department_id,manager_id,role_id)
+VALUES
+("JOHN","CHASE", 3, 5, 6),
+("TED", "MOLSBY", 1, 2, 4),
+("BOBBY", "HILL",1,1,2),
+("VICTOR", "CROSS", 1, 3, 2), 
+("SUSAN", "MEGAS", 1, 3, 2), 
+("CLARK", "KENT", 1, 3, 2), 
+("BRUCE", "WANYE", 2, 1, 3), 
+("TONY", "STARK", 3, 1, 4),
+("JEAN", "GREY", 4, 2, 6),
+("STEVE", "AUSTIN", 4, 3, 9n),
+("DIANA", "PRINCE", 5, 3, 6),
+("MATT", "LEINART", 7, 2, 1),
+("MICHAEL", "BRANDON", 8, 2, 4),
+("JIMMY", "LOGAN", 4, 3, 5),
+("REESE", "BANNER", 4, 4, 5)
+;
--- 02-Homework/tracker.js
+++ 02-Homework/tracker.js
@@ -18,6 +18,114 @@ connection.connect(function(err){
 function start(){
     inquirer
     .prompt({
-        
+        name:"action",
+        type: "rawlist",
+        message:"What are you trying to do?",
+        choices:["----ADD---",
+            "Add Employee",
+            "Add Job",
+            "Add Department",
+            "---VIEW---",
+            "View Department",
+            "View Job",
+            "View Employee",
+            "Update Employee",
+            "Exit",
+        ]
     })
+    .then(function(answer){
+        switch(answer.action){
+            case "Add Employee":
+                addEmployee();
+                break;
+                case "Add Job":
+                    addJob();
+                    break;
+                case "Add Department":
+                addDepartment();
+                break;
+                case "View Employee":
+                    viewEmployee();
+                    break;
+                case "View Job":
+                    viewJob();
+                    break;
+                case "View Department":
+                viewDepartment();
+                break;
+            case "Update Employee":
+                updateEmployee();
+                break;
+                case "Exit":
+                exit();
+                break;
+        }
+    })
+}
+function viewEmployee(){
+    var query = "SELECT first_name, last_name,title,salary, dept FROM employee INNER join role ON employee.role_id=job.id INNER join department ON job.department_id=deparment.id";
+    connection.query(query, function(err, res){
+        if (err) throw err;
+        console.table(res)
+        start();
+    });
+
+}
+function viewDepartment(){
+    var query = "SELECT * FROM department";
+    connection.query(query,function(err,res){
+        if (err) throw err;
+        console.table(res);
+        start();
+    })
+}
+function viewJob(){
+    var query = "SELECT * FROM job";
+    connection.query(query,function(err,res){
+        if (err) throw err;
+        console.table(res)
+        start();
+    });
+}
+
+function addDepartment(){
+    inquirer
+    .prompt({
+        name: "createDepartment",
+        type: "input",
+        message: "Which Department would you like to be added?"
+    })
+    .then(function(answer){
+        var query = "INSERT INTO department(dept) VALUES (?)";
+        connection.query(query, answer.createDepartment, function(err, res){
+            if (err) throw err;
+            viewDepartment();
+        });
+    });
+}
+function addRole(){
+    inquirer
+    .prompt([{
+        name:"employeeTitle",
+        type:"input",
+        message:"What's your title?"
+    },
+    {
+        name:"employeeSalary",
+        type:"input",
+        message:"What's your current salary?"
+    },
+    {
+        name:"employeeDepartmentID",
+        type:"input",
+        message:"Department ID?"
+    },
+])
+.then(function(answer){
+    var query = "INSERT INTO role(title,salary, department_id) VALUES(?,?,?)";
+    connection.query(query,[answer.emloyeeTitle,answer.employeeSalary,answer.employeeDepartmentID], function(err, res){
+        if (err) throw err;
+        viewRole();
+    });
+});
 }
